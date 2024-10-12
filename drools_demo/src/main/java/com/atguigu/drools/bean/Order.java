96cd736b394016b52b5026f3003db25bd43e27e3
tomato <2535981850@qq.com>
2024-10-12 17:23:05 +0800

    完成预估订单金额功能

---
+++ drools_demo/src/main/java/com/atguigu/drools/bean/Order.java
@@ -0,0 +1,31 @@
+package com.atguigu.drools.bean;
+
+/**
+ * ClassName: Bean
+ * Description:
+ * <p>
+ * DateTime: 2024/10/12 9:24
+ * Author: 悉达多@NWAFU
+ * Version: 1.0
+ */
+public class Order {
+
+    private double amout;
+    private double score;
+
+    public double getAmout() {
+        return amout;
+    }
+
+    public void setAmout(double amout) {
+        this.amout = amout;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
